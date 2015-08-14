2363e638d43853ab9ef8422a72543c6b4b39c5c9
Jonathan <jlvedder@aol.com>
2015-08-14 07:13:49 -0700

    test for checkpoint 23

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -75,32 +75,34 @@ RSpec.describe AddressBook do
 
      it "imports the 2nd entry" do
        book.import_from_csv("entries.csv")
-       check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
        entry_two = book.entries[1]
-       
+       check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
      end
  
      it "imports the 3rd entry" do
        book.import_from_csv("entries.csv")
-       check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
        entry_three = book.entries[2]
+       check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+       
 
      end
  
      it "imports the 4th entry" do
        book.import_from_csv("entries.csv")
-       check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
        entry_four = book.entries[3]
+       check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
+       
        
      end
  
      it "imports the 5th entry" do
        book.import_from_csv("entries.csv")
-       check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
        entry_five = book.entries[4]
+       check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+       
 
      end
      
-     end
+    end
 
 end
